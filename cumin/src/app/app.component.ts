1b774b8ca3593d6c9300767e92c0965003411ecb
Adro.codes <hurricane.interactive@gmail.com>
2018-10-03 18:51:41 +1000

    Updated app structure / imported new components & modules / fetching current user data

--- cumin/src/app/app.component.ts
+++ cumin/src/app/app.component.ts
@@ -1,15 +1,36 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'
+import axios from 'axios'
 
 @Component({
 	selector: 'app-root',
 	templateUrl: './app.component.html',
 	styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
 	private access_token = window.sessionStorage.getItem('access_token')
 	private refresh_token = window.sessionStorage.getItem('refresh_token')
+	private base_url: string = 'https://api.spotify.com/v1'
+
+	current_user: object|null = JSON.parse(window.sessionStorage.getItem('current_user')) || null;
+	opponent: object|null = JSON.parse(window.sessionStorage.getItem('opponent')) || null;
 
 	constructor() {
-		
+		axios.get(`${this.base_url}/me`, {
+			headers: {
+				'Authorization': 'Bearer ' + this.access_token
+			}
+		})
+		.then((res) => {
+			this.current_user = res.data
+			window.sessionStorage.setItem('current_user', JSON.stringify(this.current_user));
+		})
+		.catch((e) => {
+			console.error(e)
+		})
+	}
+
+	onSearch(term: string) {
+		console.log('App', term);
 	}
 }
--- cumin/src/app/app.module.ts
+++ cumin/src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { NavigationComponent } from './navigation/navigation.component';
+import { UserProfileComponent } from './user-profile/user-profile.component';
+import { UserSearchComponent } from './user-search/user-search.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavigationComponent
+    NavigationComponent,
+    UserProfileComponent,
+    UserSearchComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
