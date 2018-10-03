2ea70ec2236069aced0008b8b8b3f97203487a25
Adro.codes <hurricane.interactive@gmail.com>
2018-10-03 18:50:46 +1000

    Initial user profile component

---
+++ cumin/src/app/user-profile/user-profile.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-user-profile',
+  templateUrl: './user-profile.component.html',
+  styleUrls: ['./user-profile.component.scss']
+})
+
+export class UserProfileComponent implements OnInit {
+  @Input() user: object;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
