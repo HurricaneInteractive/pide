c09a014347622929be684cfe83385865069f5a3f
Adro.codes <hurricane.interactive@gmail.com>
2018-10-06 14:40:19 +1000

    Empty generated components for current user data

---
+++ cumin/src/app/cu-tracks-data/cu-tracks-data.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cu-tracks-data',
+  templateUrl: './cu-tracks-data.component.html',
+  styleUrls: ['./cu-tracks-data.component.scss']
+})
+export class CuTracksDataComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
