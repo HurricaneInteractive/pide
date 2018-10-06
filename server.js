b8d63a554797424c4c245a210f30bcfb59f59cf1
Adro.codes <hurricane.interactive@gmail.com>
2018-10-06 14:44:05 +1000

    Additional scope settings

--- server.js
+++ server.js
@@ -50,7 +50,7 @@ app.get('/login', function(req, res) {
 	res.cookie(stateKey, state);
   
 	// your application requests authorization
-	let scope = 'user-read-private user-read-email';
+	let scope = 'user-read-private user-read-email user-follow-read playlist-read-private playlist-read-collaborative user-library-read user-top-read user-read-recently-played';
 	res.redirect('https://accounts.spotify.com/authorize?' +
 		querystring.stringify({
 			response_type: 'code',
