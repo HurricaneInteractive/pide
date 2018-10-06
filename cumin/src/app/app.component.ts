7fb2d0f0ab50d5903f994192de777c699ab44af0
Adro.codes <hurricane.interactive@gmail.com>
2018-10-06 10:10:02 +1000

    Display search results & save opponent on click

--- cumin/src/app/app.component.ts
+++ cumin/src/app/app.component.ts
@@ -18,8 +18,10 @@ export class AppComponent {
 	private me_url: string = this.base_url + '/me'
 	private search_url: string = this.base_url + '/search'
 
-	current_user: object|null = JSON.parse(window.sessionStorage.getItem('current_user')) || null;
-	opponent: object|null = JSON.parse(window.sessionStorage.getItem('opponent')) || null;
+	current_user: object|null = JSON.parse(window.sessionStorage.getItem('current_user')) || null
+	opponent: object|null = JSON.parse(window.sessionStorage.getItem('opponent')) || null
+
+	searchResults: Array<object>|null = null;
 
 	constructor() {
 		axios.get(`${this.me_url}`, this.getAuthHeaders())
@@ -44,9 +46,21 @@ export class AppComponent {
 		axios.get(`${this.search_url}?q=${encodeURIComponent(term + '*')}&type=artist`, this.getAuthHeaders())
 			.then((res) => {
 				console.log('Res', res)
+				this.searchResults = res.data.artists.items
 			})
 			.catch((e) => {
 				console.error(e)
+			})
+	}
+
+	onArtistSelect(artist: any) {
+		this.opponent = artist
+		axios.get(`${this.base_url}/artists/${artist.id}`, this.getAuthHeaders())
+			.then((res) => {
+				this.opponent = res.data
+			})
+			.catch((e) => {
+				console.error(e);
 			})
 	}
 }
