3cef6f8fec9cd1a04ec8e70beb40f643e04a14aa
Adro.codes <hurricane.interactive@gmail.com>
2018-10-06 10:10:13 +1000

    Included new components and modules

--- cumin/src/app/app.module.ts
+++ cumin/src/app/app.module.ts
@@ -7,16 +7,23 @@ import { NavigationComponent } from './navigation/navigation.component';
 import { UserProfileComponent } from './user-profile/user-profile.component';
 import { UserSearchComponent } from './user-search/user-search.component';
 
+import { IconsModule } from './icons/icons.module';
+import { SearchResultsComponent } from './search-results/search-results.component';
+import { SearchResultItemComponent } from './search-result-item/search-result-item.component';
+
 @NgModule({
   declarations: [
     AppComponent,
     NavigationComponent,
     UserProfileComponent,
-    UserSearchComponent
+    UserSearchComponent,
+    SearchResultsComponent,
+    SearchResultItemComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    IconsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
