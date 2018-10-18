911c1a6a7a1d157eeab38fa56d1d95c4ae4c7b53
Luke Secomb <luke.secomb@live.com>
2018-10-18 12:03:46 +1100

    fixed api call - now loops to get all playlist data

--- paprika/src/App.js
+++ paprika/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import axios from 'axios'
 import './App.scss';
+// import filler from './filler.json'
 
 // https://countryflags.io/ could be used to get user location flag
 
@@ -15,12 +16,55 @@ class App extends Component {
     this.state = {
       user: null,
       user_loading: true,
-      playlists: null,
-      playlists_20: null,
+      playlists: [],
+      playlists_2: null,
+      playlists_3: null,
+      playlists_4: null,
       playlist_loading: true,
       current: null,
       current_loading: true,
+      nextCall: 'https://api.spotify.com/v1/users/12162909955/playlists?offset=50&limit=50'
     }
+  }
+
+  getPlaylistsRequest(nextOffset) {
+    console.log('this.state.nextCall => ', nextOffset)
+    axios({
+      method: 'get',
+      url: 'https://api.spotify.com/v1/users/12162909955/playlists',
+      params: {
+        offset: nextOffset,
+        limit: 50
+      },
+      headers: {
+        Authorization: "Bearer " + AUTH_TOKEN
+      }
+    }).then(res => {
+      const current = this.state.playlists;
+
+      for (let i = 0, len = res.data.items.length; i < len; i++) {
+        const addThisPlaylist = res.data.items[i];
+        current.push(addThisPlaylist);
+      }
+      console.log('res.data => ', res.data)
+      console.log('res.data.next => ', res.data.next)
+      this.setState({
+        playlists: current
+      });
+    });
+  }
+
+  getNumberOfTimesPlaylistsNeedsToBeRun(total) {
+    let runThisManyTimes = (Math.ceil(total / 50)) + 1;
+    console.warn(runThisManyTimes);
+
+    for (let i = 1; i < runThisManyTimes; i++) {
+      let nextNumber = i * 50;
+      this.getPlaylistsRequest(nextNumber)
+    }
+    this.setState({
+      playlist_loading: false
+    })
   }
 
   componentDidMount() {
@@ -33,7 +77,7 @@ class App extends Component {
       }
     }).then(res => {
       const user_data = res.data;
-      console.log("axios user => ", user_data)
+      // console.log("axios user => ", user_data)
       this.setState({
         user: user_data,
         user_loading: false
@@ -47,73 +91,66 @@ class App extends Component {
         Authorization: "Bearer " + AUTH_TOKEN
       }
     }).then(res => {
-      console.log("res.data => ", res.data)
       const current_data = res.data;
-      console.log("axios current => ", current_data)
       this.setState({
         current: current_data,
         current_loading: false
       });
     });
 
+    // playlist requests
     axios({
       method: 'get',
       url: 'https://api.spotify.com/v1/me/playlists',
       params: {
+        limit: 50,
         offset: 0
       },
       headers: {
         Authorization: "Bearer " + AUTH_TOKEN
       }
     }).then(res => {
-      const playlists = res.data;
+      console.warn('(res.data => ', res.data.items);
+      const current = this.state.playlists;
+      for (let i = 0, len = res.data.items.length; i < len; i++) {
+        const addThisPlaylist = res.data.items[i];
+        current.push(addThisPlaylist);
+      }
+
       this.setState({
-        playlists: playlists,
-        // loading: false
+        playlists: current
       });
-      axios({
-        method: 'get',
-        url: 'https://api.spotify.com/v1/me/playlists',
-        params: {
-          offset: 20
-        },
-        headers: {
-          Authorization: "Bearer " + AUTH_TOKEN
-        }
-      }).then(res => {
-        const playlists_20 = res.data;
-        this.setState({
-          playlists_20: playlists_20,
-          playlist_loading: false
-        });
-      })
+      
+      console.log('this.state.playlists => ', this.state.playlists)
+      console.log('this.state.nextCall => ', this.state.nextCall)
+      
+      this.getNumberOfTimesPlaylistsNeedsToBeRun(res.data.total);
     })
   }
 
   mapPlaylistCovers() {
     let data = this.state.playlists;
-    let data_20 = this.state.playlists_20;
-    let keys = Object.keys(data.items);
-    let keys_20 = Object.keys(data_20.items);
+    console.warn('ALL THE JUICY DATA', data);
+    console.log('ALL THE JUICY DATA', data);
+    let keys = Object.keys(data);
+    console.log('TCL: mapPlaylistCovers -> keys', keys);
+    
 
     let allPlaylistCovers = keys.map(key => {
-      const playlist_image = data.items[key].images[0].url;
-      return (
-        <img style={{width: "100%"}} src={playlist_image} key={key} alt={key}/>
-      )
-    });
+      const playlist_image = data[key].images[0] || null;
 
-    let allPlaylistCovers_20 = keys_20.map(keys_20 => {
-      const playlist_image = data_20.items[keys_20].images[0].url;
+      if (playlist_image === null) {
+        console.log('if playlist_image.images === null => ', data[key])
+        return null
+      }
       return (
-        <img style={{width: "100%"}} src={playlist_image} key={keys_20} alt={keys_20}/>
+        <img style={{width: "100%"}} src={playlist_image.url} key={key} alt={key}/>
       )
     });
 
     return (
       <div style={{display: "grid", gridTemplateColumns: "1fr 1fr 1fr 1fr 1fr"}}>
         {allPlaylistCovers}
-        {allPlaylistCovers_20}
       </div>
     )
   }
@@ -124,9 +161,9 @@ class App extends Component {
       return <p>loading</p>
     }
 
-    console.log("this.state.current => ", this.state.current)
-    console.log("AUTH_TOKEN => ", AUTH_TOKEN)
-    console.log("REFRESH_TOKEN => ", REFRESH_TOKEN)
+    // console.log("this.state.current => ", this.state.current)
+    // console.log("AUTH_TOKEN => ", AUTH_TOKEN)
+    // console.log("REFRESH_TOKEN => ", REFRESH_TOKEN)
     return (
       <div className="App">
         <header className="bg-white">
