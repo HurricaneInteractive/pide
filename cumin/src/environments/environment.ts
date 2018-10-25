f69341cc3649de33977d7bd2835e62ccad608d96
Adro.codes <hurricane.interactive@gmail.com>
2018-10-26 09:43:12 +1100

    environment

--- cumin/src/environments/environment.ts
+++ cumin/src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  lastFM: '405d3719bfcd12eb4e4986b616aaeade'
 };
 
 /*
