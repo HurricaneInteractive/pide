9e7ff72d4fcaedb0a8d50b89de1247851ace9c7c
timmytrumpit <timknott6@gmail.com>
2018-10-30 10:55:18 +1100

    bgImage fallback, progress circle, cleaning up code

--- masala/js/methods/BgImage.js
+++ masala/js/methods/BgImage.js
@@ -1,8 +1,12 @@
 export const bgImage = (data) => {
 
-  console.log(data)
-  const artistImage = data.images[0].url
+  // console.log(data)
+  let artistImage = 'https://images.unsplash.com/photo-1535554975110-9133cf938160?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=35d4b5255f124736018f06fa8487e858&w=1000&q=80'
   const div = document.getElementById('title')
+
+  if (data.images.length > 0) {
+    artistImage = data.images[0].url
+    }
 
   div.insertAdjacentHTML('afterbegin', '<img id="bg-img" src="'+artistImage+'" />');
 
