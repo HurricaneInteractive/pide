9579b779dff90bfeb29e49ec9e916dbb905c3338
timmytrumpit <timknott6@gmail.com>
2018-11-01 11:13:58 +1100

    fully responsive, hover on circles, cleaned up js

--- masala/js/methods/BgImage.js
+++ masala/js/methods/BgImage.js
@@ -1,5 +1,4 @@
 export const bgImage = (data) => {
-  console.log(data)
   let artistImage = 'https://images.unsplash.com/photo-1535554975110-9133cf938160?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=35d4b5255f124736018f06fa8487e858&w=1000&q=80'
   let artistName = data.name
   const div = document.getElementById('title')
--- masala/js/script.js
+++ masala/js/script.js
@@ -5,7 +5,7 @@ import * as circleProgress from 'jquery-circle-progress'
 import { listTracks } from './methods/ListTracks'
 import { bgImage } from './methods/BgImage'
 import { indieRating } from './methods/indieRating'
-// import { artistPop } from './methods/artistPop'
+import { artistPop } from './methods/artistPop'
 
 
 const apiUrl = 'https://api.spotify.com/v1/me/player/recently-played'
@@ -13,16 +13,6 @@ const artistUrl = 'https://api.spotify.com/v1/artists/'
 
 const AUTH_TOKEN = window.sessionStorage.access_token
 const REFRESH_TOKEN = window.sessionStorage.refresh_token
-
-// function compareGenre(response) {
-//   console.log(response.data.items[0].track.popularity)
-// }
-
-
-
-function artistPop(response) {
-  console.log(response.data.items[0].track.artists[0].name)
-}
 
 $(document).ready(() => {
   // const i = [1, 2, 3].map(n => n ** 2);
@@ -45,34 +35,36 @@ function getSpotifyData() {
     }
   })
   .then(function (response) {
-    listTracks(response);
-    // bgImage(response);
-    indieRating(response);
+    listTracks(response)
+    indieRating(response)
+    // artistPop(response)
 
-return response.data.items
+    return response.data.items
 
 
   })
   .then((items) => {
     // loop and do all the calls.
-    let artist = []
+    let promises = []
     items.forEach((item, i) => {
       let artists = item.track.artists
       if (artists.length > 0) {
         let id = artists[0].id
-        artist.push(axios.get(`${artistUrl}${id}`, { headers: { Authorization: "Bearer " + AUTH_TOKEN } }))
+        promises.push(axios.get(`${artistUrl}${id}`, { headers: { Authorization: "Bearer " + AUTH_TOKEN } }))
       }
     })
 
+
+
     // // array of promises
-    axios.all(artist).then(data => {
+    axios.all(promises).then(data => {
+      // console.log(data)
+      artistPop(data)
       data.forEach(({data}, i) => {
         // append
-
         if (i === 0) {
-          bgImage(data);
+          bgImage(data)
         }
-        // artistPop();
       })
     })
   })
@@ -81,24 +73,6 @@ return response.data.items
   });
 }
 
-$('.circle').circleProgress({
-  value: 0.75,
-  size: 200,
-  thickness: 6,
-  lineCap: "round",
-  fill: "#fff",
-  startAngle: -Math.PI / 2
+$('.artist-see-all').click(function() {
+  $('#artists').toggleClass('artist-active')
 });
-
-// function getArtistImg() {
-//   axios({
-//     method: 'get',
-//     url: artistUrl,
-//     headers: {
-//       Authorization: "Bearer " + AUTH_TOKEN
-//     }
-//   })
-//   .then(function (res) {
-//
-//   })
-// }
